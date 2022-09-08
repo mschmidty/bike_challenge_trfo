9b472c1e50da38baa58806a5ff6e441379dd16a3
Mike Schmidt <mschmidty@gmail.com>
2022-09-08 13:04:53 -0600

    made website for bike challenge

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>Tres Rios Field Office September Bike Challenge</h1>
+    </header>
+    <RouterView />
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/RiderTotals.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <section class="leader-section">
+      <h1>Most Activities</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Total Activities</th>
+          </tr>
+        </thead>
+        <tbody v-for="person in sortedByActivity" :key="person.key">
+          <tr>
+            <td>{{person.key}}</td>
+            <td>{{person.numActivities}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h1>Most Miles</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Total Miles</th>
+          </tr>
+        </thead>
+        <tbody v-for="person in sortedByMiles" :key="person.key">
+          <tr>
+            <td>{{person.key}}</td>
+            <td>{{person.numActivities}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    
+    <hr>
+    <h1>Individual Summaries</h1>
+    <p>Way to get out there everyone! Keep it up.</p>
+    <div v-for="person in riderData" :key="person.key" class="person-summary">
+      <h3>{{person.key}}</h3>
+      <p>Total Miles: {{person.totalMiles}} | Number of Activies: {{person.numActivities}}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Activity Type</th>
+            <th>Distance (Miles)</th>
+          </tr>
+        </thead>
+        
+        <tbody v-for="activity in person.values" :key="activity['Completion Time']">
+          <tr>
+            <td>{{activity["Activity Date"]}}</td>
+            <td>{{activity["Activity"]}}</td>
+            <td>{{activity["Miles"]}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {csvParse} from "d3-dsv"
+  import {tidy, groupBy, arrange, desc} from "@tidyjs/tidy"
+  
+  export default {
+    data(){
+      return {
+        riderData:[],
+        sortedByMiles:[],
+        sortedByActivity:[]
+      }
+    },
+    mounted(){
+      fetch("../public/bike_challenge_data.csv")
+        .then(response=>response.text())
+        .then(text=>{
+          const data = csvParse(text)
+          const groupedData = tidy(
+            data,
+            groupBy('Name2', groupBy.entriesObject())
+          )
+          //console.log(groupedData)
+          const finalData = groupedData.map(person=>{
+            person["numActivities"]=person["values"].length
+            //Total Miles
+            const arrMiles = person["values"].map(activities=>{
+              return parseFloat(activities.Miles)
+            })
+            person["totalMiles"] = arrMiles.reduce((a,b)=>{
+              return a+b
+            }, 0)
+            return person
+          })
+          this.riderData=finalData
+
+          this.sortedByMiles = tidy(
+            finalData,
+            arrange(desc('totalMiles'))
+          )
+          this.sortedByActivity=tidy(
+            finalData,
+            arrange([desc('numActivities')])
+          )
+        })
+    }
+   
+  }
+</script>
+
+<style lang="scss" scoped>
+  .person-summary{
+    margin-top: 3rem;
+  }
+</style>
